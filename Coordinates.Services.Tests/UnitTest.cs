4c0a794eb0b66cca6bf8f27df21c9afb70e839cb
Adam <adam.przybyszewski@student.put.poznan.pl>
2016-05-19 19:05:18 +0200

    Visualisation Page, UI.Tests and Services.Tests created
    -created View, Interface and ViewModel for VisualisationPage
    -registered VisualisationPage ViewModel
    -added it to hamburger menu
    -created Coordinates.UI.Tests
    -created Coordinates.Services.Tests

---
+++ Coordinates.Services.Tests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Coordinates.Services.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

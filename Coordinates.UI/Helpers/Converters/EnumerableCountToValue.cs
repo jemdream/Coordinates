0efe02a5345c4f3c9121573797d7e9463da7c6a2
Jemdream <jemdream@gmail.com>
2016-10-29 18:59:29 +0200

    [UI] [1ElementMeasurement] One element measurement

---
+++ Coordinates.UI/Helpers/Converters/EnumerableCountToValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Coordinates.UI.Helpers.Converters
+{
+    public class EnumerableCountToValue : IValueConverter
+    {
+        public int Count { get; set; }
+        public object Value { get; set; } = Visibility.Visible;
+        public object DefaultValue { get; set; } = Visibility.Collapsed;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var ienum = value as IEnumerable<object>;
+            if (ienum == null) return DefaultValue;
+
+            var condition = ienum.Count() == Count;
+
+            return condition ?
+                Value :
+                DefaultValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

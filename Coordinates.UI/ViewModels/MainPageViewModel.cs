a7762a3d6c3a4c11774bd7b24652a701d547634e
Jemdream <jemdream@gmail.com>
2016-11-03 19:49:42 +0100

    [LandingPage] Navigation from main page

--- Coordinates.UI/ViewModels/MainPageViewModel.cs
+++ Coordinates.UI/ViewModels/MainPageViewModel.cs
@@ -1,71 +1,13 @@
 using Template10.Mvvm;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Template10.Services.NavigationService;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
 using Coordinates.UI.ViewModels.Interfaces;
 
 namespace Coordinates.UI.ViewModels
 {
     public class MainPageViewModel : ViewModelBase, IMainPageViewModel
     {
-        private string _value = "Gas";
-        private DelegateCommand _gotoDetailsPage;
-        private DelegateCommand _gotoSettingPage;
-        private DelegateCommand _gotoPrivacy;
-        private DelegateCommand _gotoAbout;
-
-        public MainPageViewModel()
-        {
-            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled) // _ things for designer
-            {
-                Value = "Designtime value";
-            }
-        }
-
-        public string Value { get { return _value; } set { Set(ref _value, value); } }
-
-
-        public ICommand GotoDetailsPage =>
-            _gotoDetailsPage ?? (_gotoDetailsPage = new DelegateCommand(() =>
-                NavigationService.Navigate(typeof(Views.DetailPage), Value)));
-
-        public ICommand GotoSettings =>
-            _gotoSettingPage ?? (_gotoSettingPage = new DelegateCommand(() =>
-                NavigationService.Navigate(typeof(Views.SettingsPage), 0)));
-
-        public ICommand GotoPrivacy =>
-            _gotoPrivacy ?? (_gotoPrivacy = new DelegateCommand(() =>
-                NavigationService.Navigate(typeof(Views.SettingsPage), 1)));
-
-        public ICommand GotoAbout =>
-            _gotoAbout ?? (_gotoAbout = new DelegateCommand(() =>
-                NavigationService.Navigate(typeof(Views.SettingsPage), 2)));
-
-        #region Navigation
-        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
-        {
-            if (suspensionState.Any()) Value = suspensionState[nameof(Value)]?.ToString();
-
-            await Task.CompletedTask;
-        }
-
-        public override async Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
-        {
-            if (suspending) suspensionState[nameof(Value)] = Value;
-
-            await Task.CompletedTask;
-        }
-
-        public override async Task OnNavigatingFromAsync(NavigatingEventArgs args)
-        {
-            args.Cancel = false;
-
-            await Task.CompletedTask;
-        }
-        #endregion
+        private DelegateCommand<Page> _goTo;
+        public DelegateCommand<Page> GoTo => _goTo ?? 
+            (_goTo = new DelegateCommand<Page>(page => NavigationService.Navigate(page.GetType())));
     }
 }
-
